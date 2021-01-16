f9f1bb07f59415495c3b3e79a813a3698ab9cc69
abdulkadircelik <abdulkadircelik267@gmail.com>
2021-01-16 21:58:48 +0300

    Add project files.

---
+++ CarGalery.Entities/Concrete/Car.cs
@@ -0,0 +1,30 @@
+﻿using CarGalery.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        
+        public int CarId { get; set; }
+        public string Model { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public Car()
+        {
+
+        }
+        public Car(string model, decimal price)
+        {
+            Model = model;
+            UnitPrice = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{CarId,-5} {Model,-30} {UnitPrice,-10}";
+        }
+    }
+}
