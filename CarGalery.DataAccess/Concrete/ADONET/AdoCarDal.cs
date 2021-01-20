aa36d04113e2ddc99815b59fe45c7264e102409f
abdulkadircelik <abdulkadircelik267@gmail.com>
2021-01-20 23:05:53 +0300

    Adonet alteration..

--- CarGalery.DataAccess/Concrete/ADONET/AdoCarDal.cs
+++ CarGalery.DataAccess/Concrete/ADONET/AdoCarDal.cs
@@ -1,7 +1,9 @@
 ﻿using CarGalery.DataAccess.Abstract;
 using CarGalery.Entities.Concrete;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -11,36 +13,81 @@ namespace CarGalery.DataAccess.Concrete.ADONET
     {
         public void Add(Car entity)
         {
-            throw new NotImplementedException();
+
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO CarDB (CarId,Model,UnitPrice) " +
+                "VALUES(@CarId,@Model,@UnitPrice)"))
+            {
+                cmd.Parameters.AddWithValue("CarId", entity.CarId);
+                cmd.Parameters.AddWithValue("Model", entity.Model);
+                cmd.Parameters.AddWithValue("UnitPrice", entity.UnitPrice);
+                VTYS.SqlExecuteNonQuery(cmd);
+            }
         }
 
         public void Delete(Car entity)
         {
-            throw new NotImplementedException();
+            using (SqlCommand cmd =
+                new SqlCommand("DELETE FROM CarDB WHERE CarId = @CarId"))
+            {
+                cmd.Parameters.AddWithValue("CarId", entity.CarId);
+                VTYS.SqlExecuteNonQuery(cmd);
+            }
         }
 
         public Car Get(Expression<Func<Car, bool>> filter)
         {
-            throw new NotImplementedException();
+            List<Car> carList = new List<Car>();
+            SqlCommand cmd = new SqlCommand("Select * from CarDB");
+
+            SqlDataReader reader = VTYS.SqlExecuteReader(cmd);
+            while (reader.Read())
+            {
+                Car car = new Car
+                {
+                    CarId = Convert.ToInt32(reader[0]),
+                    Model = reader[1].ToString(),
+                    UnitPrice = Convert.ToInt32(reader[2]),
+                    
+                };
+
+                carList.Add(car);
+            }
+            var list = carList.Where(filter.Compile()).ToList();
+            return list[0];
         }
 
         public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
-            List<Car> cars = new List<Car>
-            {
-                new Car{CarId=1,Model="Ford Mustang",UnitPrice=1150000},
-                new Car{CarId=2,Model="Fiat Egea",UnitPrice=185000},
-                new Car{CarId=3,Model="Mercedes S400",UnitPrice=2000000},
-                new Car{CarId=4,Model="BMW 320",UnitPrice=500000},
-                new Car{CarId=5,Model="Ford Focus",UnitPrice=350000}
+            List<Car> CarList = new List<Car>();
+            SqlCommand cmd = new SqlCommand("Select * from CarDB");
 
-            };
-            return cars;
+            SqlDataReader reader = VTYS.SqlExecuteReader(cmd);
+            while (reader.Read())
+            {
+                Car car = new Car
+                {
+                    CarId = Convert.ToInt32(reader[0]),
+                    Model = reader[1].ToString(),
+                    UnitPrice = Convert.ToInt32(reader[2]),
+                    
+                };
+
+                CarList.Add(car);
+            }
+
+            return filter == null ? CarList : CarList.Where(filter.Compile()).ToList();
         }
             public void Update(Car entity)
             {
-                throw new NotImplementedException();
+            using (SqlCommand cmd = new SqlCommand("UPDATE CarDb set CarId = @CarId," +
+                " Model=@Model, UnitPrice=@UnitPrice WHERE CarId = @CarId"))
+            {
+                cmd.Parameters.AddWithValue("@CarId", entity.CarId);
+                cmd.Parameters.AddWithValue("@Model", entity.Model);
+                cmd.Parameters.AddWithValue("@UnitPrice", entity.UnitPrice);
+                VTYS.SqlExecuteNonQuery(cmd);
             }
+        }
         }
     }
 
